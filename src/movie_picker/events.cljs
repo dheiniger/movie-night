f29ee710a8944072fe211ae1debe56b876f9cf56
Daniel Heiniger <daniel.r.heiniger@gmail.com>
2022-09-01 22:09:47 -0500

    Add basic login page

--- src/movie_picker/events.cljs
+++ src/movie_picker/events.cljs
@@ -26,14 +26,14 @@
     (print "initializing db... " event)
     (let [db (second event)
           api-key props/movie-api-key]
-    {:db         (assoc db :state :loading)
-     :http-xhrio {:method          :get
-                  ;;:uri             "http://localhost:3001/movies"
-                  :uri             (str "https://imdb-api.com/en/API/Top250Movies/" api-key)
-                  :timeout         8000
-                  :response-format (ajax/json-response-format {:keywords? true})
-                  :on-success      [:database-initialized]
-                  :on-failure      [:http-error-occurred]}})))
+      {:db         (assoc db :state :loading)
+       :http-xhrio {:method          :get
+                    ;;:uri             "http://localhost:3001/movies"
+                    :uri             (str "https://imdb-api.com/en/API/Top250Movies/" api-key)
+                    :timeout         8000
+                    :response-format (ajax/json-response-format {:keywords? true})
+                    :on-success      [:database-initialized]
+                    :on-failure      [:http-error-occurred]}})))
 
 (re-frame/reg-event-fx
   :search-requested
@@ -41,19 +41,24 @@
     (let [key props/movie-api-key]
       {:db         (assoc db :state :loading)
        :http-xhrio {:method          :get
-                    :uri (str "https://imdb-api.com/en/API/Search/" key "/inception")
+                    :uri             (str "https://imdb-api.com/en/API/Search/" key "/inception")
                     :timeout         8000
                     :response-format (ajax/json-response-format {:keywords? true})
                     :on-success      [:database-initialized]
                     :on-failure      [:http-error-occurred]}})))
 
+(re-frame/reg-event-fx
+  :attempt-login
+  (fn [db credentials]
+    (prn "credentials are " credentials)))
+
 (re-frame/reg-event-db
   :database-initialized
   (fn [db [_ event]]
     (let [movies (:items event)]
-    (print "good result - " movies)
-    (assoc db :state :not-loading
-              :movies movies))))
+      (print "good result - " movies)
+      (assoc db :state :not-loading
+        :movies movies))))
 
 (re-frame/reg-event-db
   :http-error-occurred
@@ -86,5 +91,22 @@
     (prn "current db before adding movie input " db)
     (assoc db :current-movie-input (second movie))))
 
-(defn x[]
-  ())
+(re-frame/reg-event-db
+  :navigate
+  (fn [db link]
+    (prn "Navigating to " (second link))
+    (assoc db :page (second link))))
+
+(re-frame/reg-event-db
+  :attempt-login
+  (fn [db credentials]
+    (let [username (first (rest credentials))
+          password (second (rest credentials))]
+      (prn "logging in... " username password)
+      (if (and
+            (= "Daniel" username)
+            (= "p" password))
+        (assoc db :username username :error-messages nil)
+        (assoc db :error-messages ["Invalid username - try again"])))))
+
+
